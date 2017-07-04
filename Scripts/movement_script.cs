c514d6ff3177594be9fc89475a0f51a6dc302b09
thealexmax <alejandrovillarrealayala@gmail.com>
2017-07-04 19:47:04 +0200

    Update movement_script.cs

--- Scripts/movement_script.cs
+++ Scripts/movement_script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class movement_script : MonoBehaviour {
+	
+	//Variables velocidad
 	public float speed = 0.1;
 	public float nspeed = -0.1;
 
@@ -14,27 +16,27 @@ public class movement_script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey ("d"))
+		if(Input.GetKey ("d")) //Checks if the "d" button is pressed
         {
             Flip("right");
             transform.Translate(speed, 0f, 0f);
         }
-        if(Input.GetKey ("a"))
+        if(Input.GetKey ("a")) //Checks if the "a" button is pressed
         {
             Flip("left");
             transform.Translate(nspeed, 0f, 0f);
         }
 	}
-    public void Flip(string direction)
+    public void Flip(string direction) //Function flip
     {
         var thescale = transform.localScale;
-        if (direction == "right")
+        if (direction == "right") //If direction = right the sprite resets to its normal state
         {
             thescale.x = 0.297211f;
         }
         else
         {
-            thescale.x = -0.297211f;
+            thescale.x = -0.297211f; //If direction isnt = to right the sprite flips
         }
         transform.localScale = thescale;
     }
