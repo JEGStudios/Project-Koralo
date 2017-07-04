3f38b54be5eb1be3fa1cb63f5e3e0760438f81db
thealexmax <alejandrovillarrealayala@gmail.com>
2017-07-04 17:52:37 +0200

    Add files via upload

---
+++ Scripts/movement_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey ("d"))
+        {
+            Flip("right");
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        if(Input.GetKey ("a"))
+        {
+            Flip("left");
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+	}
+    public void Flip(string direction)
+    {
+        var thescale = transform.localScale;
+        if (direction == "right")
+        {
+            thescale.x = 0.297211f;
+        }
+        else
+        {
+            thescale.x = -0.297211f;
+        }
+        transform.localScale = thescale;
+    }
+}
