ebe27210e6be726036eb4e7802331aa508e8de7e
thealexmax <alejandrovillarrealayala@gmail.com>
2017-07-04 17:02:44 +0200

    Add files via upload

---
+++ Scripts/menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour {
+     public void ChangeScene(string intro_scene)
+    {
+        SceneManager.LoadScene(intro_scene);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
