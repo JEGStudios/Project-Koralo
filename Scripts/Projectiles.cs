2295220d92a65fd8905a8fbe51d733cd34115bb3
thealexmax <alejandrovillarrealayala@gmail.com>
2017-07-24 12:03:04 +0200

    Add files via upload

---
+++ Scripts/Projectiles.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+    public float speed = 1.5f;
+    private Vector3 target;
+    public GameObject projectilePrefab;
+    private List<GameObject> Projectiles = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        target = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = (GameObject)Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Projectiles.Add(bullet);
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        for(int i = 0; i < Projectiles.Count; i++)
+        {
+            GameObject goBullet = Projectiles[i];
+            if(goBullet != null)
+            {
+                Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint(goBullet.transform.position);
+                goBullet.transform.Translate(new Vector3(target.x, target.y) * Time.deltaTime * speed);
+                if(bulletScreenPos.y >= Screen.height || bulletScreenPos.x == Screen.height)
+                {
+                    DestroyObject(goBullet);
+                    Projectiles.Remove(goBullet);
+                }
+                if(bulletScreenPos.x >= Screen.width || bulletScreenPos.x == Screen.width)
+                {
+                    DestroyObject(goBullet);
+                    Projectiles.Remove(goBullet);
+                }
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "enemy")
+        {
+            Debug.Log("OnCollisionEnter2D was called!");
+        }
+    }
+}
