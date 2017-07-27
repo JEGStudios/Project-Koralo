7a2b9d91e76d43413e394d0895f78ec74379ff6e
thealexmax <alejandrovillarrealayala@gmail.com>
2017-07-27 21:07:02 +0200

    I want to kill my self

---
+++ Scripts/blacelion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blacelion : MonoBehaviour {
+
+	private Transform player;
+	public GameObject protagonist;
+
+	// Use this for initialization
+	void Start () {
+		player = protagonist.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = player.position;
+		playerpos.z = transform.position.z;
+		transform.position = playerpos;
+	}
+}
