0421ecd24cdb866022a21e69328bffe9600a7910
thealexmax <alejandrovillarrealayala@gmail.com>
2017-08-08 12:05:31 +0200

    Nah, this only works for the main camera

---
+++ Scripts/camera_controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour {
+
+	private Transform player;
+	public GameObject playerSprite;
+
+	// Use this for initialization
+	void Start () {
+		player = playerSprite.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = player.position;
+		playerpos.z = transform.position.z;
+		transform.position = playerpos;
+	}
+}
